8b226faa41bd2213691bae8f634289cc3636b8e0
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-09-22 17:10:36 -0300

    add main components

---
+++ src/components/JobListing.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { defineProps, ref, computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  job: Object
+})
+
+// Variable to toggle full description
+const showFullDescription = ref(false)
+
+// Method to toggle full description
+const toggleDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
+
+// Computed property to show full description
+const fullDescription = computed(() => {
+  return showFullDescription.value
+    ? props.job.description
+    : props.job.description.slice(0, 90) + '...'
+})
+</script>
+
+<template>
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">
+          {{ job.type }}
+        </div>
+        <h3 class="text-xl font-bold">{{ job.title }}</h3>
+      </div>
+
+      <div class="mb-5">
+        <p>{{ fullDescription }}</p>
+        <button
+          type="button"
+          @click="toggleDescription"
+          class="text-green-500 hover:text-green-600 text-sm"
+        >
+          {{ showFullDescription ? 'Read Less' : 'Read More' }}
+        </button>
+      </div>
+
+      <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
+
+      <div class="border border-gray-100 mb-5"></div>
+
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <div class="text-orange-700 mb-3">
+          <i class="pi pi-map-marker text-orange-700 mr-1"></i>
+          <span>{{ job.location }}</span>
+        </div>
+        <RouterLink
+          :to="'/jobs/' + job.id"
+          class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+        >
+          Read More
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
